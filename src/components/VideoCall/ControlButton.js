b5c926011ee30746ef0b5a8cc75196f20ed5b7f3
kasemsan00 <kasemsan.cho@gmail.com>
2023-02-09 09:43:26 +0700

    dev

--- src/components/VideoCall/ControlButton.js
+++ src/components/VideoCall/ControlButton.js
@@ -21,7 +21,7 @@ export default function ControlButton({ handleClick, isActive, textActive, textU
     <div className="mx-1">
       <motion.button
         className="flex flex-1 flex-col justify-center items-center rounded-xl
-        mobileSE:w-18 mobile:w-18 sm:w-24 md:w-24 lg:w-24 h-[60px] w-[60px]"
+        mobileSE:w-18 mobile:w-18 sm:w-24 md:w-24 lg:w-24 h-[60px] w-[60px] px-[35px]"
         onClick={handleClick}
         ref={controlButtonRef}
         whileTap={{ scale: 0.9 }}
