7b7b334bd51adc04a8fe04a5231bddcc4e6f16c7
kasemsan00 <kasemsan.cho@gmail.com>
2023-02-09 13:39:03 +0700

    .

--- src/components/LinkCall/LinkCall.js
+++ src/components/LinkCall/LinkCall.js
@@ -24,9 +24,9 @@ export const LinkCall = ({ uuid, extensionStatus }) => {
       <div className="flex flex-1 h-[calc(100vh-100px)] justify-center items-center">
         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.3 }}>
           <motion.div
-            className="bg-[#D13A2E] w-[30vh] h-[30vh] rounded-full shadow-md drop-shadow-md 
-                                    shadow-gray-700 text-2xl text-white flex flex-1 justify-center items-center 
-                                    text-center self-center cursor-pointer"
+            className="bg-[#D13A2E] w-[30vh] h-[30vh] rounded-full shadow-md drop-shadow-md
+            shadow-gray-700 text-2xl text-white flex flex-1 justify-center items-center
+            text-center self-center cursor-pointer"
             onClick={handleCall}
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
