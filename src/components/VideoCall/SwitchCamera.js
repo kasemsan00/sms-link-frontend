fb2aee4e4ec469f03d88f918a6dacc78bea63742
Kasemsan <kasemsan.cho@gmail.com>
2023-03-14 16:45:00 +0700

    .

--- src/components/VideoCall/SwitchCamera.js
+++ src/components/VideoCall/SwitchCamera.js
@@ -5,6 +5,37 @@ import { setControlSwitchCamera } from "../../redux/slices/controlVideoSlice";
 import useTranslation from "next-translate/useTranslation";
 import useIsomorphicLayoutEffect from "use-isomorphic-layout-effect";
 
+const FrontCamera = () => {
+  const { t } = useTranslation("common");
+  return (
+    <>
+      <Image
+        src={require("../../assets/videocall/btn_camera_switch_back.png")}
+        width={40}
+        height={40}
+        alt="Camera Back"
+        className="switch-camera-image"
+      />
+      <div className="switch-camera-text">{t("front-camera")}</div>
+    </>
+  );
+};
+const BackCamera = () => {
+  const { t } = useTranslation("common");
+  return (
+    <>
+      <Image
+        src={require("../../assets/videocall/btn_camera_switch_front.png")}
+        width={40}
+        height={40}
+        alt="Camera Front"
+        className="switch-camera-image"
+      />
+      <div className="switch-camera-text">{t("back-camera")}</div>
+    </>
+  );
+};
+
 export default function SwitchCamera() {
   const switchCameraRef = useRef(null);
   const dispatch = useDispatch();
@@ -34,29 +65,7 @@ export default function SwitchCamera() {
       className="top-[45px] right-0 w-[60px] h-[60px] fixed flex flex-col justify-center items-center cursor-pointer z-50] rounded-md"
       ref={switchCameraRef}
     >
-      {facingMode === "user" || facingMode === "" ? (
-        <>
-          <Image
-            src={require("../../assets/videocall/btn_camera_switch_back.png")}
-            width={40}
-            height={40}
-            alt="Camera Back"
-            className="switch-camera-image"
-          />
-          <div className="switch-camera-text">{t("front-camera")}</div>
-        </>
-      ) : (
-        <>
-          <Image
-            src={require("../../assets/videocall/btn_camera_switch_front.png")}
-            width={40}
-            height={40}
-            alt="Camera Front"
-            className="switch-camera-image"
-          />
-          <div className="switch-camera-text">{t("back-camera")}</div>
-        </>
-      )}
+      {facingMode === "user" || facingMode === "" ? <BackCamera /> : <FrontCamera />}
     </div>
   );
 }
