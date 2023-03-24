35fa65801420a47ad005877662a165677f7c6be6
kasemsan00 <kasemsan.cho@gmail.com>
2023-03-24 08:23:55 +0700

    dev

--- src/pages/survey.js
+++ src/pages/survey.js
@@ -1,14 +1,28 @@
-const SurveyButton = ({ point }) => {
-  return <button>{point}</button>;
+import { useState } from "react";
+// import Footer from "../components/Utilities/Footer";
+// import StatusbarGeo from "../components/Status/StatusBarGeo";
+
+const SurveyButton = ({ point, handleClickSetPoint }) => {
+  return <button onClick={() => handleClickSetPoint(point)}>{point}</button>;
 };
 
 export default function Survey() {
+  const [point, setPoint] = useState(null);
+  const handleClickSetPoint = (value) => {
+    setPoint(value);
+  };
   return (
     <div className="h-screen w-screen flex flex-1 flex-col justify-center items-center">
+      {/*<StatusbarGeo show={true} uuid={"SSS"} />*/}
       <div className="text-2xl">ประเมินความพึงพอใจ</div>
-      <div>
-        <SurveyButton point={1} />
+      <div className="flex gap-4">
+        <SurveyButton point={1} handleClickSetPoint={handleClickSetPoint} />
+        <SurveyButton point={2} handleClickSetPoint={handleClickSetPoint} />
+        <SurveyButton point={3} handleClickSetPoint={handleClickSetPoint} />
+        <SurveyButton point={4} handleClickSetPoint={handleClickSetPoint} />
+        <SurveyButton point={5} handleClickSetPoint={handleClickSetPoint} />
       </div>
+      {/*<Footer />*/}
     </div>
   );
 }
